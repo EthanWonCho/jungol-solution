46fec3c87d435c77428ee9f88d7865aea5f582d1
EthanWonCho <realethanwoncho@gmail.com>
2024-11-26 21:20:43 +0900

    New solutions

--- 1671_paper_jung/main.cpp
+++ 1671_paper_jung/main.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
+
 using namespace std;
+
 int arr[110][110];
 
+int dir[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
 int main(void) {
-    int n, c = 0;
-    cin >> n;
-    for(int m=0;m<n;++m) {
-        int a, b;
-        cin >> a >> b;
-        for(int i=a+1;i<=a+10;++i) {
-            for(int j=b+1;j<=b+10;++j) {
-                arr[i][j] = 1;
+    int paper_n;
+    cin >> paper_n;
+    for(int i=0;i<paper_n;++i) {
+        int x, y;
+        cin >> x >> y;
+        for(int j=x;j<x+10;++j) {
+            for(int k=y;k<y+10;++k) {
+                arr[j][k] = 1;
             }
         }
     }
-    for(int i=0;i<=100;++i) {
-        for(int j=0;j<=100;++j) {
-            cout << arr[i][j];
-            if(arr[i][j]) {
-                if(!arr[i-1][j])
-                    ++c;
-                if(!arr[i+1][j])
-                    ++c;
-                if(!arr[i][j-1])
-                    ++c;
-                if(!arr[i][j+1])
-                    ++c;
+    int rst = 0;
+    for(int i=0;i<100;++i) {
+        for(int j=0;j<100;++j) {
+            if(arr[i][j]){
+                for(int k=0;k<4;++k) {
+                    int ni = i + dir[k][0];
+                    int nj = j + dir[k][1];
+                    if(!arr[ni][nj])
+                        ++rst;
+                }
             }
         }
-        cout << endl;
     }
-    cout << c << endl;
+    cout << rst << endl;
     return 0;
 }
 
